17797ecc32bff1efe85d125e4f802095bb8c8a21
Rhuan Costanzo de Souza <rhuancostanzo@hotmail.com>
2023-01-18 23:32:16 -0300

    first-commit

---
+++ src/app.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { JogadoresModule } from './jogadores/jogadores.module';
+import { CategoriasModule } from './categorias/categorias.module';
+import { DesafiosModule } from './desafios/desafios.module';
+
+@Module({
+  imports: [
+    MongooseModule.forRoot('mongodb+srv://admin:a1caXratbSaWcvBY@cluster0.te5jzd5.mongodb.net/smartranking?retryWrites=true&w=majority',
+    { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: false }),
+    JogadoresModule,
+    CategoriasModule,
+    DesafiosModule],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
